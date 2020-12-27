99bee6ac8742a7e8a0361990dfd103b2c714336a
Giorgio Previtera <giorgioprevitera@gmail.com>
2020-12-27 22:16:15 +0000

    Add test to part one

---
+++ 1/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartOne(t *testing.T) {
+	input := []int{
+		1721,
+		979,
+		366,
+		299,
+		675,
+		1456,
+	}
+	expectedOutput := 514579
+
+	actualOutput, err := searchAnswerPartOne(input, 2020)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, expectedOutput, actualOutput)
+}
